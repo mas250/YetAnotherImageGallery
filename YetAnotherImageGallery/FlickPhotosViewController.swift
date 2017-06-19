19a3fa00c7fa8abd21540ea121a4cb7e0a0b2b0b
mas250 <mas250@exeter.ac.uk>
2017-06-19 07:40:48 +0100

    Comments added

--- YetAnotherImageGallery/FlickPhotosViewController.swift
+++ YetAnotherImageGallery/FlickPhotosViewController.swift
@@ -10,9 +10,8 @@ import UIKit
 
 final class FlickrPhotosViewController : UICollectionViewController {
     
-    //
-    fileprivate let reuseIdentifier = "FlickrCell"
 
+fileprivate let reuseIdentifier = "FlickrCell"  //UICollectionView requires for cell reuse
 fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
 fileprivate var searches = [FlickrSearchResults]()
 fileprivate let flickr = Flickr()
@@ -20,7 +19,7 @@ fileprivate let itemsPerRow: CGFloat = 3
 
     
 }
-//
+
 private extension FlickrPhotosViewController{
     func photoForIndexPath(indexPath: IndexPath) -> FlickrPhoto { //
         return searches [(indexPath as NSIndexPath).section].searchResults[(indexPath as IndexPath).row]
@@ -29,7 +28,7 @@ private extension FlickrPhotosViewController{
 
 extension FlickrPhotosViewController : UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        //
+        // log status to console. Should also alert user.
         let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
         textField.addSubview(activityIndicator)
         activityIndicator.frame = textField.bounds
@@ -47,11 +46,11 @@ extension FlickrPhotosViewController : UITextFieldDelegate {
             }
             
             if let results = results {
-                //
+                //add to search array and update UI. Results can be reordered here.
                 print("Found \(results.searchResults.count) matching \(results.searchTerm)")
                 self.searches.insert(results, at: 0)
                 
-                //
+                
                 self.collectionView?.reloadData()
             }
         }
@@ -62,18 +61,18 @@ extension FlickrPhotosViewController : UITextFieldDelegate {
 }
 
 extension FlickrPhotosViewController {
-    //
+    //Returns one header per set of search results. Design could be changed to allow one header per result.
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return searches.count
         }
     
-    //
+    
     override func collectionView( _ collectionView: UICollectionView, numberOfItemsInSection section : Int) -> Int {
     return searches[section].searchResults.count
     }
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind : String, at indexPath: IndexPath) -> UICollectionReusableView {
         
-        //
+        //Populate header with search term.
         switch kind {
         case UICollectionElementKindSectionHeader:
             
@@ -87,18 +86,14 @@ extension FlickrPhotosViewController {
     
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
-        //
+        //Populate cell with value for photo.
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! FlickrPhotoCell
         
-        //
         let flickrPhoto = photoForIndexPath(indexPath: indexPath)
         cell.backgroundColor = UIColor.white
         
-
-        
-        //
         cell.imageView.image = flickrPhoto.thumbnail
-        //cell.label.text = flickrPhoto.Photo
+        //cell.label.text = flickrPhoto.Photo // Extract metadata to display alongside photo.
         
         return cell
     }
@@ -108,9 +103,8 @@ extension FlickrPhotosViewController {
 }
 
 extension FlickrPhotosViewController : UICollectionViewDelegateFlowLayout {
-    //
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        //
+        //Format search results to display on screen.
         let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
         let availableWidth = view.frame.width - paddingSpace
         let widthPerItem = availableWidth / itemsPerRow
